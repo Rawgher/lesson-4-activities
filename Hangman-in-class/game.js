521758b4c5ed86dc7a5119525da15a07a42cd74d
email <phupeter916@gmail.com>
2018-06-23 13:54:15 -0700

    firt push

--- Hangman-in-class/game.js
+++ Hangman-in-class/game.js
@@ -1,1 +1,36 @@
-var
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var lettersUsed = [];
+var displayedWord = [];
+var wrongLetters = [];
+var words = ["fast", "slow", "tall", "short", "javascript"];
+var randomWord = Math.floor(Math.random() * words.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guessesLeft;
+document.getElementById("lettersUsed").innerHTML = "Letters already used: " + lettersUsed;
